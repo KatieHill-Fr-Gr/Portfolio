6e914246db0301c50672e447b930c8ed9e1fadd1
Katie Hill <khill.fr.gr@gmail.com>
2025-10-21 11:17:16 +0100

    User urls tested & updated

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
-    # path('users/', include('users.urls')),
+    path('api/', include('users.urls')),
     path('technologies/', include('technologies.urls'))
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from users.views import SignInView, UserUpdateView
 
 urlpatterns = [
-    path('api/auth/admin/', SignInView.as_view()),
+    path('auth/admin/', SignInView.as_view()),
     path('<int:pk>/', UserUpdateView.as_view())
 ]
 
