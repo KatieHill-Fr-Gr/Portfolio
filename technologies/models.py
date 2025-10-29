ea3a8e9be585594ad2215890981afdabbd9f3edd
Katie Hill <khill.fr.gr@gmail.com>
2025-10-29 21:18:32 +0000

    Category field added to model

--- technologies/models.py
+++ technologies/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Technology(models.Model):
     name = models.CharField(max_length=100)
     icon = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
