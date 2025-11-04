f7cbd69e3240fee24e5439e860fa63627c3efa1c
Katie Hill <khill.fr.gr@gmail.com>
2025-11-04 15:36:08 +0000

    User endpoints corrected

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from users.views import SignInView, UserUpdateView, UsersListView
 
 urlpatterns = [
     path('auth/admin/', SignInView.as_view()),
-    path('users/', UsersListView.as_view(), name='users-list'),
-    path('users/<int:pk>/', UserUpdateView.as_view()),
+    path('', UsersListView.as_view(), name='users-list'),
+    path('<int:pk>/', UserUpdateView.as_view()),
 ]
 
